6b2307506601f2014bd7c4386928f86ab700882e
Anantika09 <anantikaag06@gmail.com>
2025-07-22 23:52:41 +0530

    Create emergencyContactModel.js

---
+++ models/emergencyContactModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const emergencyContactSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  phone: { type: String, required: true },
+  relationship: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+});
+
+module.exports = mongoose.model('EmergencyContact', emergencyContactSchema);
